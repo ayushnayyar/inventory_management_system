2042679cc448de63f4436cc1d8a644bc56e74e18
Abhishek Lale <abhisheklale181@gmail.com>
2021-01-21 10:02:40 +0530

    done with db schema

---
+++ database/migrations/2021_01_21_041848_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('party_id')->unsigned();
+            $table->integer('item_id')->unsigned();
+            $table->integer('stock');
+            $table->integer('current_stock');
+            $table->integer('beam');
+            $table->integer('dispatched');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
