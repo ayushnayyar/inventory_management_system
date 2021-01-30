38d5cd876253b844f7631c4da7eb1d4aecfc9178
Abhishek Lale <abhisheklale181@gmail.com>
2021-01-30 22:38:33 +0530

    db schema added

---
+++ database/migrations/2021_01_30_170418_create_materials_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->string('material_name');
+            $table->string('material_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
