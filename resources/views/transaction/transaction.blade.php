ab4d91c11171d1d04670d89b385a728bb9341c61
Abhishek Lale <abhisheklale181@gmail.com>
2021-02-25 13:20:30 +0530

    displayed transactions

--- resources/views/transaction/transaction.blade.php
+++ resources/views/transaction/transaction.blade.php
@@ -2,24 +2,25 @@
 @section('content')
 <div class="container">
 <h1>Transaction</h1>
-<table id="customers" class='table table-dark table-hover table-responsive-sm'>
+<table id="customers"  class="table table-dark">
       <tr>
       <th>Order id</th>
         <th>Item ID</th>
         <th>Vendor ID</th>
-        <th>Initial Stock</th>
-        <th>Current Stock</th>
+        <th>Stock initial</th>
+        <th>Current stock</th>
         <th>Beam</th>
         <th>Dispatch</th>
       </tr>
-      <tr class='table-activer'>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany item</td>
-        <td>Germany jsjf</td>
-        <td>Germany sdffgssfg</td>
-        <td>Germany dd</td>
-        <td><a href="" class="btn btn-warning">Update</a></td>
+      @foreach($transactions as $transaction)
+      <tr>
+        <td>{{ $transaction->order_id}} </td>
+        <td>{{ $transaction->party_id }}</td>
+        <td>{{ $transaction->item_id }} </td>
+        <td>{{ $transaction->stock }} </td>
+        <td>{{ $transaction->current_stock }} </td>
+        <td>{{ $transaction->beam }} </td>
+        <td> {{ $transaction->dispatched }} </td>
       </tr>
      @endforeach
   </table>
