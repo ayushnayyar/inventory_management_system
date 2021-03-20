74ef7834b9cd15c99eb7085ead3149c7c555a33d
ayushnayyar <ayushnayyar48@gmail.com>
2021-03-20 12:24:18 +0530

    Changed Margins for dashboard cards and heading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <div class='heading'>
-        <h1>Dashboard</h1>
+        <h1 class='ml-3'>Dashboard</h1>
     </div>
 
         <div class='row'>
-            <div class="card border-primary" style="width: 16rem; margin: 10px 25px;">
+            <div class="card border-primary ml-3 mr-3 mb-3 mt-3" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">Vendor Details</h5>
                     <p class="card-text">Registered Vendors: 12</p>
@@ -15,7 +15,7 @@
                 <div class='card-footer border-primary bg-transparent'>
                 <a href="#" class="btn btn-primary">View Vendors</a></div>
             </div>
-            <div class="card border-primary" style="width: 16rem; margin: 10px 25px;">
+            <div class="card border-primary ml-3 mr-3 mb-3 mt-3" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">Material Details</h5>
                     <p class="card-text">Available Materials: 15</p>
@@ -23,7 +23,7 @@
                 <div class='card-footer border-primary bg-transparent'>
                 <a href="#" class="btn btn-primary">View Materials</a></div>
             </div>
-            <div class="card border-primary" style="width: 16rem; margin: 10px 25px;">
+            <div class="card border-primary ml-3 mr-3 mb-3 mt-3" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">Order Details</h5>
                     <p class="card-text">Orders yet to be fulfilled: 2</p>
@@ -32,7 +32,7 @@
                 <div class='card-footer border-primary bg-transparent'>
                 <a href="#" class="btn btn-primary">View Orders</a></div>
             </div>
-            <div class="card border-primary" style="width: 16rem; margin: 10px 25px;">
+            <div class="card border-primary ml-3 mr-3 mb-3 mt-3" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">Transactions</h5>
                     <p class="card-text">Transactions today: 3</p>
