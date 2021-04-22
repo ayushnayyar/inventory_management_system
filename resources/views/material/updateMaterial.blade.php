e4a0452510a6ed45088d669f6b97fe8ec2c2e550
Riya Jain <66663462+riaaajain@users.noreply.github.com>
2021-04-22 15:18:37 +0530

    changes done in both udate pages

--- resources/views/material/updateMaterial.blade.php
+++ resources/views/material/updateMaterial.blade.php
@@ -1,17 +1,54 @@
 @extends('layouts.app')
 @section('content')
 <div id="layoutSidenav_content">
-    <form  action="{{ Route('material.update', [$material->id]) }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label class='mt-3' for="name">Item Name</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Material Name" name="material_name" value="{{ $material->material_name }}">
-        </div>
-        <div class="form-group">
-            <label class='mt-3' for="name">Item type</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Material Type" name="material_type" value="{{ $material->material_type }}">
-        </div>
-            <input type="submit" class="btn btn-primary mt-3" name="submit">
-    </form>
+    <div>
+
+        <h4 class="aligns mt-5">Update Material</h4>
+
+        <form  action="{{ Route('material.update', [$material->id]) }}" method="POST">
+            @csrf
+            <div class="form-group col-4">
+                <label class='mt-3' for="name">Item Name</label>
+                <input type="text" class="form-control input-back-color" id="exampleInputEmail1" placeholder="Enter Material Name" name="material_name" value="{{ $material->material_name }}">
+            </div>
+            <div class="form-group col-4">
+                <label class='mt-3' for="name">Item type</label>
+                <input type="text" class="form-control input-back-color" id="exampleInputEmail1" placeholder="Enter Material Type" name="material_type" value="{{ $material->material_type }}">
+            </div>
+            <div class="col-4">
+                <button type="submit" class="d-block btn submit-button-color">Submit</button>
+            </div>
+        </form>
+        {{-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Update Material</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                    <form  action="{{ Route('material.update', [$material->id]) }}" method="POST">
+                        @csrf
+                        <div class="form-group col-4">
+                            <label class='mt-3' for="name">Item Name</label>
+                            <input type="text" class="form-control input-back-color" id="exampleInputEmail1" placeholder="Enter Material Name" name="material_name" value="{{ $material->material_name }}">
+                        </div>
+                        <div class="form-group col-4">
+                            <label class='mt-3' for="name">Item type</label>
+                            <input type="text" class="form-control input-back-color" id="exampleInputEmail1" placeholder="Enter Material Type" name="material_type" value="{{ $material->material_type }}">
+                        </div>
+                        
+                    </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="submit" class="d-block btn submit-button-color">Submit</button>
+                </div>
+              </div>
+            </div>
+          </div> --}}
+    </div>
 </div>
 @endsection
--- resources/views/vendor/updateVendor.blade.php
+++ resources/views/vendor/updateVendor.blade.php
@@ -3,23 +3,25 @@
 <div id="layoutSidenav_content">
     <div>
 
-        <h4>update Vendor</h4>
+        <h4 class="aligns mt-5">Update Vendor</h4>
         
         <form action="{{ Route('vendor.update', [$vendor->id]) }}" method="POST">
             @csrf
-            <div class="form-group">
+            <div class="form-group col-4">
                 <label class='mt-3' for="name">Vendor Name</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Vendor Name" name="party_name" value="{{ $vendor->party_name }}">
+                <input type="text" class="form-control input-back-color"  id="exampleInputEmail1" placeholder="Enter Vendor Name" name="party_name" value="{{ $vendor->party_name }}">
             </div>
-            <div class="form-group">
+            <div class="form-group col-4">
                 <label class='mt-3' for="name">GST No.</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter GST No." name="gst_no" value="{{ $vendor->gst_no }}">
+                <input type="text" class="form-control input-back-color "  id="exampleInputEmail1" placeholder="Enter GST No." name="gst_no" value="{{ $vendor->gst_no }}">
             </div>
-            <div class="form-group">
+            <div class="form-group col-4">
                 <label class='mt-3' for="name">Vendor Address</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Vendor Address" name="address" value="{{ $vendor->address }}">
+                <input type="text" class="form-control input-back-color" id="exampleInputEmail1" placeholder="Enter Vendor Address" name="address" value="{{ $vendor->address }}">
             </div>
-            <input type="submit" class="btn btn-primary mt-3" name="submit">
+            <div class="col-4">
+                <button type="submit" class="d-block btn submit-button-color">Submit</button>
+            </div>
         </form>
     </div>
     </div>
